092ac7168bce87ae3d1e5bb3beb396ab07f5bfd3
Steffen Buehl <steffen.buehl@aberle-software.com>
2018-11-20 20:20:01 +0100

    check for failure in stream closing event

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ var builder = require("junit-report-builder");
 var program = require("commander");
 
 program
-  .version("0.1.0")
-  .option("-n, --name [name]", "Set name for testsuide", "ABAPLint")
-  .option("-f, --file [file]", "Set report filename", "report.xml")
-  .parse(process.argv);
+    .version("0.1.0")
+    .option("-n, --name [name]", "Set name for testsuide", "ABAPLint")
+    .option("-f, --file [file]", "Set report filename", "report.xml")
+    .parse(process.argv);
 
 var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  terminal: false
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
 });
 
 var suite = builder.testSuite().name(program.name);
@@ -20,24 +20,23 @@ var suite = builder.testSuite().name(program.name);
 var failure = false;
 //array.forEach(line => {
 rl.on("line", line => {
-  let obj = new Error(line);
-  if (obj.isValid()) {
-    var testCase = suite
-      .testCase()
-      .className(obj.getClassname())
-      .name(obj.line)
-      .error("ERR: " + obj.error)
-      .failure(obj.error);
+    let obj = new Error(line);
+    if (obj.isValid()) {
+        var testCase = suite
+            .testCase()
+            .className(obj.getClassname())
+            .name(obj.line)
+            .error("ERR: " + obj.error)
+            .failure(obj.error);
 
-    failure = true;
-    console.log(line);
-  }
+        failure = true;
+        console.log(line);
+    }
 });
 
 rl.on("close", () => {
-  builder.writeTo(program.file);
+    builder.writeTo(program.file);
+    if (failure) {
+        process.exit(1);
+    }
 });
-
-if (failure) {
-  process.exit(1);
-}
